5cfc6efe8ec3d91aa56da761e21c0ca12433e20b
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-30 21:44:23 +0300

    task

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,12 +2,12 @@
 """
 encrypt_password.py
 """
-
 import bcrypt
 
-def hash_password(password):
+
+def hash_password(password: str) -> bytes:
     """expects one string argument name password
     and returns a salted, hashed password, which is a byte string."""
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode(), salt)
+    salt: bytes = bcrypt.gensalt()
+    hashed: bytes = bcrypt.hashpw(password.encode(), salt)
     return hashed
